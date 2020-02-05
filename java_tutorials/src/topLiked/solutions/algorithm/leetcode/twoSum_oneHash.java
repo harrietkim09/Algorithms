31b3d7780f5ccea47a6504383d2889086c162fe6
Harriet Kim <h.kim8@newcastle.ac.uk>
2020-02-05 22:19:14 +0000

    updated test code

--- java_tutorials/src/topLiked/solutions/algorithm/leetcode/twoSum_oneHash.java
+++ java_tutorials/src/topLiked/solutions/algorithm/leetcode/twoSum_oneHash.java
@@ -8,9 +8,13 @@ public class twoSum_oneHash {
     public int [] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
 
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] {i, map.get(complement)};
+            }
+            map.put(nums[i], i);
         }
-        int[] a = {1,2,3};
-        return a;
+        return new int[]{};
     }
 }
